646401123a7bd5085e83ca466ecfc24c0f048933
NepNeppedYuumi <post4wouter.t@gmail.com>
2023-12-01 23:33:57 +0100

    made day 1 more readable and efficient

--- 001.py
+++ 001.py
@@ -1,36 +1,18 @@
+
+
 with open('001.txt', 'r') as file:
     lines = []
-    dicto = {
-        'one': 1,
-        'two': 2,
-        'three': 3,
-        'four': 4,
-        'five': 5,
-        'six': 6,
-        'seven': 7,
-        'eight': 8,
-        'nine': 9
-    }
-    count = -1
-    line_num = 3
     for line in file:
-        count += 1
-        if count < line_num:
-            continue
         line2 = ''
-        print(line)
         for i in range(0, len(line)):
             if line[i].isnumeric():
                 line2 += line[i]
                 continue
-            for key, value in dicto.items():
-                if line[i:i + 5].startswith(key):
-                    line2 += str(value)
-        line = line2
-        line = line[0] + line[-1]
-        line = int(line)
+            for y, value in enumerate(('one','two','three','four',
+                                       'five','six','seven','eight','nine'), start=1):
+                if line[i:i + 5].startswith(value):
+                    line2 += str(y)
+                    break
+        line = int(line2[0] + line2[-1])
         lines.append(line)
-        print(line)
-        if count == line_num:
-            break
     print(sum(lines))
