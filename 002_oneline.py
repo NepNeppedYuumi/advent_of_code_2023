5f0d5690c50bd9c85b021560d1935a897ac19cfa
NepNeppedYuumi <post4wouter.t@gmail.com>
2023-12-02 19:11:32 +0100

    added piere's solution added example nested for loops

--- 002_oneline.py
+++ 002_oneline.py
@@ -25,3 +25,22 @@ print(sum(
 print(sum(map(lambda x:int(x[0].split(' ')[-1]) if not any((True for col in x[1].replace(';', ',').split(', ') if (12, 13, 14)[('red', 'green', 'blue').index(col.split(' ')[-1])] < int(col.split(' ')[0])))else 0,map(lambda x: x.strip().split(': '), open('002.txt', 'r')))))
 
 
+# pièrre's solution
+
+print(sum(
+    int(g.split(' ')[1])
+    for g,r in
+    (l.strip().split(': ')
+     for l in open('input.txt'))
+    if all({"red":13,"green":14,"blue":15}.get(
+        c.split(' ')[1], 0) > int(c.split(' ')[0]
+                               ) for s in r.split("; ")
+           for c in s.split(", ")
+           )
+    )
+)
+
+# test for 1 line nested for loops
+
+gen = (i for y in ((1, 2), (3, 4), (5, 6)) for i in y)
+
