ebe88fed693f8f775386827ee77c76542cf15e6d
NepNeppedYuumi <post4wouter.t@gmail.com>
2023-12-05 22:50:23 +0100

    committed txt's

--- 005.py
+++ 005.py
@@ -28,30 +28,79 @@
 from datetime import datetime
 
 # part 2
-with open('005.txt', 'r') as file:
+with open('day05.txt', 'r') as file:
     seeds = file.readline().split(": ")[1].rstrip().split(' ')
     seeds = list(map(int, seeds))
-    lowest = 1000000000000000000000
+    lowest = []
     print(seeds)
+    seed_range = []
     for i in range(0, len(seeds), 2):
-        actual_seeds = list(range(seeds[i], seeds[i+1] + seeds[i]))
-        print(f"{i} out of {int(len(seeds) / 2)} at {datetime.now()}")
-        instructions = file.read().strip().split("\n\n")
-        for i in instructions:
-            i = i.split("\n")
-            i_name = i[0]
-            new_seeds = []
-            maps = []
-            for new, old, length in map(str.split, i[1:]):
-                new, old, length = int(new), int(old), int(length)
-                ra = (old - new, old, old + length)
-                maps.append(ra)
-            for seed in actual_seeds[:]:
-                for diff, start, stop in maps:
-                    if seed in range(start, stop):
-                        new_seeds.append(seed - diff)
-                        actual_seeds.remove(seed)
-            actual_seeds.extend(new_seeds)
-        lowest = min(min(actual_seeds), lowest)
-        print(lowest)
+        seed_range.append(range(seeds[i], seeds[i+1] + seeds[i]))
+
+    instructions = file.read().strip().split("\n\n")
+    for y, i in enumerate(instructions):
+        print(
+            f"instruction {int(y)} out of 6")
+        print(seed_range)
+        i = i.split("\n")
+        i_name = i[0]
+        maps = []
+        for new, old, length in map(str.split, i[1:]):
+            new, old, length = int(new), int(old), int(length)
+            ra = (old - new, old, old + length)
+            maps.append(ra)
+        print(maps)
+        new_ranges = []
+        for diff, old_start, old_end in maps:
+            change_map = range(old_start, old_end)
+            current_index = 0
+            while current_index < len(seed_range):
+                current_range = seed_range[current_index]
+                if not current_range:
+                    print(current_range)
+                    seed_range.pop(current_index)
+                    continue
+                first = current_range[0]
+                last = current_range[-1]
+                if first in change_map or last in change_map:
+                    # print(current_range, change_map, diff, seed_range)
+                    seed_range.pop(current_index)
+                    if first not in change_map:
+                        first_id = current_range.index(change_map[0])
+                        seed_range.append(current_range[:first_id])
+
+                        new_ranges.extend([range(current_range[first_id] - diff, last - diff)])
+                    elif last not in change_map:
+                        last_id = current_range.index(change_map[-1]) + 1
+
+                        seed_range.append(current_range[last_id:])
+                        new_ranges.extend([range(first - diff, current_range[last_id] - diff)])
+                    else:
+                        new_ranges.extend(
+                            [range(first - diff, first - diff + len(current_range))]
+                        )
+
+                    current_index -= 1
+
+                elif change_map[0] in current_range and change_map[-1] in current_range:
+                    # print(change_map, diff, seed_range)
+                    this_start = current_range.index(change_map[0])
+                    seed_range.append(current_range[:this_start])
+                    seed_range.append(current_range[this_start+length:])
+                    new_ranges.append(current_range[this_start:this_start+length])
+                    seed_range.pop(current_index)
+                    current_index -= 1
+
+                elif change_map[0] in current_range or change_map[-1] in current_range:
+                    raise BrokenPipeError
+                else:
+                    pass
+                current_index += 1
+
+        seed_range.extend(new_ranges)
+
+    # lowest.append(min(seed_range, key=lambda x: x[0])[0])
+    lowest.extend(seed_range)
+    actual_lowest = min(lowest, key=lambda x:x[0])
     print(lowest)
+    print(actual_lowest[0])
