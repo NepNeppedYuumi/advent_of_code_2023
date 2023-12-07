2deb4496527a40ec4583dbd5dcaff93854b7680d
NepNeppedYuumi <post4wouter.t@gmail.com>
2023-12-07 20:25:44 +0100

    Added the missed files

--- 006/006_p2.py
+++ 006/006_p2.py
@@ -1,24 +1,6 @@
 
-# part 1
-# with open('006', 'r') as file:
-#     times, distances = file.read().split('\n')
-# times, distances = times[11:].split(), distances[11:].split()
-# product = 1
-# for time, distance in zip(map(int, times), map(int, distances)):
-#     counter = 0
-#     for x in range(1, time):
-#
-#         if x * (time - x) > distance:
-#             print(x, x * (time - x), distance)
-#             counter += 1
-#     else:
-#         print(counter)
-#         product *= counter
-# print(product)
-
-
 # part 2
-with open('006', 'r') as file:
+with open('', 'r') as file:
     times, distances = file.read().split('\n')
 to_number = lambda x: int(x[11:].replace(' ', ''))
 time, distance = to_number(times), to_number(distances)
@@ -40,7 +22,7 @@ for x in range(time, 1, -1):
 print(end - start + 1)
 
 
-with open('006', 'r') as file:
+with open('', 'r') as file:
     times, distances = file.read().split('\n')
 to_number = lambda x: int(x[11:].replace(' ', ''))
 time, distance = to_number(times), to_number(distances)
