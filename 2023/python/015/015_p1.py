4fa9cf998bdc9c72721746c004b1c5ea71b84c22
NepNeppedYuumi <post4wouter.t@gmail.com>
2023-12-16 12:22:07 +0100

    day 15

--- 2023/python/015/015_p1.py
+++ 2023/python/015/015_p1.py
@@ -3,15 +3,25 @@ from itertools import combinations
 
 def parser(file_name="input.txt"):
     with open(file_name, 'r') as file:
-        lines = file.read().splitlines()
+        lines = file.read().strip().split(',')
     return lines
 
 
-def p1():
-    return None
+def p1(data):
+    total = 0
+    for word in data:
+        num = 0
+        for c in word:
+            num += ord(c)
+            num *= 17
+            num = num % 256
+        total += num
+        print(num)
+    print(total)
+    return total
 
 
-test_data = parser("011_test.txt")
-assert p1(test_data) == 374
+test_data = parser("015_test.txt")
+assert p1(test_data) == 1320
 actual_data = parser()
 print(p1(actual_data))
--- test.py
+++
@@ -1,9 +0,0 @@
-from functools import cache
-
-
-@cache
-def func(thing):
-    return thing[1]
-
-
-func({1, 2, 3})
